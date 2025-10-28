dadb17d9d448d6610fef1a4cd8e5505b5073100e
Rahul Kumar <rahulkumar22.bd@gmail.com>
2025-10-29 00:52:16 +0600

    2nd commit

---
+++ src/test/java/pages/LogInPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class LogInPage {
+    public String loginPageURL = "https://www.saucedemo.com/v1/index.html";
+
+    public String loginPageTitle = "Swag Labs";
+
+    public String usernameBox = "//input[@id='user-name']";
+
+    public String emailInputBox = "//input[@id='password']";
+
+    public String errorMsg = "//h3[normalize-space()='Epic sadface: Username is required']";
+
+    public String loginButton = "//input[@id='login-button']";
+}
